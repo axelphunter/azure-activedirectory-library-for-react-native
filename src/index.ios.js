22afa7492f3fd05d31f6606cd6cdc023cb53ff1d
Axel Hunter <axel@axelhunter.net>
2018-09-20 10:00:38 +0100

    added return to configure method

--- src/index.ios.js
+++ src/index.ios.js
@@ -1,29 +1,34 @@
 /**
  * Created by durgaprasad on 7/17/17.
  */
-import {
-    NativeModules,
-    PermissionsAndroid
-} from 'react-native';
+import { NativeModules, PermissionsAndroid } from 'react-native'
 
-const {
-    RNAzureAdal
-} = NativeModules;
+const { RNAzureAdal } = NativeModules
 
-import BaseAzureAdal from './BaseAzureAdal';
+import BaseAzureAdal from './BaseAzureAdal'
 
 export default class AzureAdal extends BaseAzureAdal {
-    /**
-     *
-     * @param authority
-     * @param validateAuthority
-     * @param clientId
-     * @param redirectUrl
-     * @param useBroker if true, it will try to use broker based authentication only if broker is present
-     */
-    configure (authority: String, validateAuthority: Boolean, clientId: String,
-               redirectUrl: String, useBroker: Boolean) {
-        RNAzureAdal.configure(authority, validateAuthority, clientId, redirectUrl, useBroker);
-    }
+  /**
+   *
+   * @param authority
+   * @param validateAuthority
+   * @param clientId
+   * @param redirectUrl
+   * @param useBroker if true, it will try to use broker based authentication only if broker is present
+   */
+  configure(
+    authority: String,
+    validateAuthority: Boolean,
+    clientId: String,
+    redirectUrl: String,
+    useBroker: Boolean
+  ) {
+    return RNAzureAdal.configure(
+      authority,
+      validateAuthority,
+      clientId,
+      redirectUrl,
+      useBroker
+    )
+  }
 }
-
